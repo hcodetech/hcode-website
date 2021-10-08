936007c242b84ab4b6519bfaa0d6b124d01283cd
Aman <aman.goyal@hcode.tech>
2021-10-08 15:10:24 +0530

    Resolved Responsive Issue

--- pages/about.js
+++ pages/about.js
@@ -14,133 +14,132 @@ function about() {
         <title>About Us | Hcode Techonolgies </title>
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
-      <section className="new-container ">
-        <div className="text-center font-poppins sm:w-10/12  mx-auto py-4">
-          <h6 className="pt-4 pb-2 mt-16">About Us</h6>
+      <section className='new-container '>
+        <div className='text-center font-poppins sm:w-10/12  mx-auto py-4'>
+          <h6 className='pt-4 pb-2 mt-16'>About Us</h6>
           <h1
-            className="font-semibold text-3xl  md:text-5xl md:leading-tight  md:w-10/12 xl:w-4/6 mx-auto"
+            className='font-semibold text-3xl  md:text-5xl md:leading-tight  md:w-10/12 xl:w-4/6 mx-auto'
             dangerouslySetInnerHTML={{ __html: about_page.hero_heading }}
           />
           <p
-            className="font-normal text-gray-900 py-4  md:w-10/12 xl:w-5/6 mx-auto"
+            className='font-normal text-gray-900 py-4  md:w-10/12 xl:w-5/6 mx-auto'
             dangerouslySetInnerHTML={{ __html: about_page.hero_paragraph }}
           />
         </div>
       </section>
-      <div className="max-w-screen-xl   mx-auto pt-4">
+      <div className='max-w-screen-xl   mx-auto pt-4'>
         <img
           src={about_page.hero_image}
-          layout="fill"
-          className="min-w-full min-h-[400px] object-cover"
+          layout='fill'
+          className='min-w-full min-h-[400px] object-cover'
         />
       </div>
-      <section className="new-container bg-gray-50 pt-4">
-        <div className="font-poppins sm:w-10/12 md:w-10/12 xl:w-1/2 max-w-[700px]  mx-auto py-5 md:py-10  ">
-          <h1 className=" text-center font-semibold text-3xl pt-10  md:text-4xl md:leading-tight">
+      <section className='new-container bg-gray-50 pt-4'>
+        <div className='font-poppins sm:w-10/12 md:w-10/12 xl:w-1/2 max-w-[700px]  mx-auto py-5 md:py-10  '>
+          <h1 className=' text-center font-semibold text-3xl pt-10  md:text-4xl md:leading-tight'>
             {about_page.heading_2}
           </h1>
           <p
-            className="font-light mt-5 mb-5 leading-relaxed md:text-lg"
+            className='font-light mt-5 mb-5 leading-relaxed md:text-lg'
             dangerouslySetInnerHTML={{ __html: about_page.paragraph_2 }}
           />
-          <h1 className=" text-center pt-10 font-semibold text-3xl  md:text-4xl md:leading-tight">
+          <h1 className=' text-center pt-10 font-semibold text-3xl  md:text-4xl md:leading-tight'>
             {about_page.heading_3}
           </h1>
           <p
-            className="font-light mt-5 mb-5 leading-relaxed md:text-lg"
+            className='font-light mt-5 mb-5 leading-relaxed md:text-lg'
             dangerouslySetInnerHTML={{ __html: about_page.paragraph_3 }}
           />
         </div>
       </section>
-      <div className="new-container grid grid-cols-12 gap-4 bg-gray-50 py-2 md:pt-0 lg:pb-20">
-        <div className="col-span-12 md:col-span-4">
-          <div className="relative lg:h-[500px] w-full ">
+      <div className='new-container grid grid-cols-12 gap-4 bg-gray-50 py-2 md:pt-0 lg:pb-20'>
+        <div className='col-span-12 md:col-span-4'>
+          <div className='relative lg:h-[500px] w-full '>
             <img
-              src="/assets/img/hcode_about_us_vertical.jpg"
-              layout="fill"
-              className="min-w-full"
+              src='/assets/img/hcode_about_us_vertical.jpg'
+              layout='fill'
+              className='min-w-full'
             />
           </div>
         </div>
-        <div className="col-span-12 md:col-span-8">
-          <div className="relative h-full lg:h-[500px]">
+        <div className='col-span-12 md:col-span-8'>
+          <div className='relative h-full lg:h-[500px]'>
             <img
-              src="/assets/img/hcode_about_us_horizontal.jpg"
-              layout="fill"
-              className="min-w-full"
+              src='/assets/img/hcode_about_us_horizontal.jpg'
+              layout='fill'
+              className='min-w-full'
             />
           </div>
         </div>
       </div>
-      <section className="new-container pt-14  text-center">
-        <h1 className="font-poppins text-center font-semibold text-3xl  md:text-4xl md:leading-tight">
+      <section className='new-container pt-14  text-center'>
+        <h1 className='font-poppins text-center font-semibold text-3xl  md:text-4xl md:leading-tight'>
           Our Core Team
         </h1>
-        <div className="grid grid-cols-2 gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-7">
+        <div className='grid grid-cols-2 gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-7'>
           {TEAM.map(({ name, image, bio, link }, index) => (
             <Team key={index} name={name} image={image} bio={bio} link={link} />
           ))}
         </div>
         <div>
-          <h6 className="font-semibold py-5 text-lg">
+          <h6 className='font-semibold py-5 text-lg'>
             Interested in joining the Hcode family?
           </h6>
-          <a className=" mt-20  primary-outline" href="https://in.indeed.com/cmp/Hcode-Technologies/jobs" target="_blank">
+          <a
+            className=' mt-20  primary-outline'
+            href='https://in.indeed.com/cmp/Hcode-Technologies/jobs'
+            target='_blank'
+          >
             Check Out Open Opportunities
           </a>
         </div>
       </section>
 
       {/* Office  View */}
-      <section className="md:new-container grid grid-cols-12 lg:mt-24">
-        {/* TODO */}
-        <div className="col-span-12 lg:col-span-6 z-20 bg-blue-200 px-7 py-10 md:px-10 md:py-14 lg:max-h-[700px] lg:max-w-[500px] mt-20 md:rounded-lg">
+      <section className='md:new-container grid grid-cols-12 lg:mt-24'>
+        <div className='col-span-12 lg:col-span-6 z-20 bg-blue-200 px-7 py-10 md:px-10 md:py-14 lg:max-h-[700px] lg:max-w-[500px] mt-20 md:rounded-lg'>
           <h2
-            className="text-3xl font-semibold md:w-3/4 "
+            className='text-3xl font-semibold md:w-3/4 '
             dangerouslySetInnerHTML={{ __html: about_page.office_heading }}
           />
           <p
-            className="leading-normal font-light mt-4"
+            className='leading-normal font-light mt-4'
             dangerouslySetInnerHTML={{
               __html: about_page.office_paragraph,
             }}
           />
-          <div className="mt-3">
-
-            <Link href="/work_with_us">
-              <div className="mt-auto py-3 border-b pb-2 border-black max-w-[180px]  opacity-80 hover:opacity-100 flex cursor-pointer   ">Want to work with us
-                <ArrowRightIcon className="w-4 h-4 ml-2 mt-1" />
+          <div className='mt-3'>
+            <Link href='/work_with_us'>
+              <div className='mt-auto py-3 border-b pb-2 border-black max-w-[180px]  opacity-80 hover:opacity-100 flex cursor-pointer   '>
+                Want to work with us
+                <ArrowRightIcon className='w-4 h-4 ml-2 mt-1' />
               </div>
             </Link>
           </div>
         </div>
-        <div className=" hidden lg:block relative col-span-12 lg:col-span-6 ">
-          <img
-            src={about_page.office_1}
-            layout="fill"
-            className="rounded-lg"
-          />
-          <div className="hidden xl:block absolute rounded-lg h-[400px] w-[300px] -bottom-3/4 xl:-bottom-5 z-20 -ml-24 border-none">
+        <div className=' hidden lg:block relative col-span-12 lg:col-span-6 '>
+          <img src={about_page.office_1} layout='fill' className='rounded-lg' />
+          <div className='hidden xl:block absolute rounded-lg h-[400px] w-[300px] -bottom-3/4 xl:-bottom-5 z-20 -ml-24 border-none'>
             <img
               src={about_page.office_2}
-              layout="fill"
-              className="rounded-lg"
+              layout='fill'
+              className='rounded-lg'
             />
           </div>
         </div>
       </section>
 
       {/* Services Card */}
-      <section className="new-container mt-20 ">
+      <section className='new-container mt-20 '>
         <ServicesCard heading={true} />
       </section>
 
       {/* Client Section */}
-      <div className="my-14 new-container">
+      <div className='my-14 new-container'>
         <Client data={client} />
       </div>
       {/* CTA */}
-      <div className="new-container mb-10">
+      <div className='new-container mb-10'>
         <CTA />
       </div>
     </>
--- pages/work_with_us.js
+++ pages/work_with_us.js
@@ -24,100 +24,107 @@ function work_with_us() {
         <title>Work With Us | Hcode Techonolgies</title>
       </Head>
       {/* Hero Section */}
-      <section className="relative bg-no-repeat bg-contain ">
-        <div className="new-container  grid grid-cols-1 lg:grid-cols-2  ">
-          <div className="py-10 md:py-24 md:pr-32 mt-20">
-            <h1 className="text-3xl lg:text-4xl font-semibold">
+      <section className='relative bg-no-repeat bg-contain '>
+        <div className='new-container  grid grid-cols-1 lg:grid-cols-2  '>
+          <div className='py-10 md:py-24 md:pr-32 mt-20'>
+            <h1 className='text-3xl lg:text-4xl font-semibold'>
               {work_with_us_content.hero_heading}
             </h1>
             <p
-              className="py-5 text-lg font-light"
+              className='py-5 text-lg font-light'
               dangerouslySetInnerHTML={{
                 __html: work_with_us_content.hero_paragraph,
               }}
             />
             <a
-              href="https://in.indeed.com/cmp/Hcode-Technologies/jobs"
-              target="_blank"
-              className="primary-outline ">See Open Positions</a>
+              href='https://in.indeed.com/cmp/Hcode-Technologies/jobs'
+              target='_blank'
+              className='primary-outline '
+            >
+              See Open Positions
+            </a>
           </div>
-          <div className="relative sm:h-[322px] w-full lg:w-[558px] lg:mt-28 ">
+          <div className='relative sm:h-[322px] w-full lg:w-[558px] lg:mt-28 '>
             <img
               src={work_with_us_1}
-              layout="fill"
-              className="rounded-lg object-cover shadow-xl"
+              layout='fill'
+              className='rounded-lg object-cover shadow-xl'
             />
           </div>
         </div>
-        <div className="hidden  lg:grid grid-cols-12 -mt-48">
-          <div className="col-start-5 col-end-10 relative sm:h-[322px] w-[500px] mt-12 ">
+        <div className='hidden  lg:grid grid-cols-12 -mt-48'>
+          <div className='col-start-5 col-end-10 relative sm:h-[322px] w-[500px] mt-12 '>
             <img
               src={work_with_us_2}
-              layout="fill"
-              className="rounded-lg  shadow-xl object-cover"
+              layout='fill'
+              className='rounded-lg  shadow-xl object-cover'
             />
           </div>
-          <div className="col-start-10 col-end-12 relative sm:h-[322px] w-[256px] ">
+          <div className='col-start-10 col-end-12 relative sm:h-[322px] w-[256px] '>
             <img
               src={work_with_us_3}
-              layout="fill"
-              className="rounded-lg  shadow-xl object-cover"
+              layout='fill'
+              className='rounded-lg  shadow-xl object-cover'
             />
           </div>
         </div>
       </section>
       {/* Campus Overview */}
-      <section className="new-container mt-10">
-        <div className="sm:mt-10">
-          <h1 className="text-4xl  font-semibold font-poppins">
+      <section className='new-container mt-10'>
+        <div className='sm:mt-10'>
+          <h1 className='text-4xl  font-semibold font-poppins'>
             {work_with_us_content.campus_heading}
           </h1>
         </div>
         <p
-          className="mt-4 md:w-3/4 leading-snug font-light text-lg"
+          className='mt-4 md:w-3/4 leading-snug font-light text-lg'
           dangerouslySetInnerHTML={{
             __html: work_with_us_content.campus_paragarph,
           }}
         />
 
-        <div className="grid sm:grid-cols-2 md:w-3/4 pt-10">
-          <div className="">
-            <img src={karnal} alt="" />
-            <h4 className="text-xl font-semibold py-2">Karnal</h4>
-            <a href="https://in.indeed.com/cmp/Hcode-Technologies/jobs" target="_blank" className="pb-1 border-b inline-flex border-black hover:scale-100  cursor-pointer opacity-50 hover:opacity-100">
+        <div className='grid sm:grid-cols-2 md:w-3/4 pt-10'>
+          <div className=''>
+            <img src={karnal} alt='' />
+            <h4 className='text-xl font-semibold py-2'>Karnal</h4>
+            <a
+              href='https://in.indeed.com/cmp/Hcode-Technologies/jobs'
+              target='_blank'
+              className='pb-1 border-b inline-flex border-black hover:scale-100  cursor-pointer opacity-50 hover:opacity-100'
+            >
               See positions in Karnal, India
-              <ArrowRightIcon className="w-6 ml-2 " />
+              <ArrowRightIcon className='w-6 ml-2 ' />
             </a>
           </div>
-          <div className="mt-8 sm:mt-0">
-            <img src={remote} alt="" />
-            <h4 className="text-xl font-semibold  py-2">Remote</h4>
-            <a href="https://in.indeed.com/cmp/Hcode-Technologies/jobs" target="_blank" className="pb-1 border-b inline-flex border-black hover:scale-100  cursor-pointer opacity-50 hover:opacity-100">
+          <div className='mt-8 sm:mt-0'>
+            <img src={remote} alt='' />
+            <h4 className='text-xl font-semibold  py-2'>Remote</h4>
+            <a
+              href='https://in.indeed.com/cmp/Hcode-Technologies/jobs'
+              target='_blank'
+              className='pb-1 border-b inline-flex border-black hover:scale-100  cursor-pointer opacity-50 hover:opacity-100'
+            >
               See positions in Remote
-              <ArrowRightIcon className="w-6 ml-2 " />
+              <ArrowRightIcon className='w-6 ml-2 ' />
             </a>
           </div>
         </div>
       </section>
-      {/* Image Carousel TODO */}
-      <div className="py-10 md:py-4">
+      {/* Image Carousel */}
+      <div className='py-10 md:py-4'>
         <ImageCarousel />
       </div>
       {/* What we offer */}
-      <section className="new-container bg">
-        <h1 className="font-semibold text-5xl mt-2 mb-4">
-          What we offer
-        </h1>
-        <div className="grid grid-cols-12">
+      <section className='new-container bg'>
+        <h1 className='font-semibold text-5xl mt-2 mb-4'>What we offer</h1>
+        <div className='grid grid-cols-12'>
           {what_we_do.map((item, index) => (
-            <div className="col-span-12 mb-7 md:mb-0 md:col-span-6">
+            <div className='col-span-12 mb-7 md:mb-0 md:col-span-6'>
               <img src={item.img} alt={item.heading} />
-              <h2 className="font-semibold text-2xl mt-3">{item.heading}</h2>
-              <ul className="list-disc list-inside mt-2">
+              <h2 className='font-semibold text-2xl mt-3'>{item.heading}</h2>
+              <ul className='list-disc list-inside mt-2'>
                 {item.points.map((li, index) => (
-                  <li>
-                    {li.point}
-                  </li>
+                  <li>{li.point}</li>
                 ))}
               </ul>
             </div>
@@ -125,38 +132,35 @@ function work_with_us() {
         </div>
       </section>
       {/* Benefits */}
-      <section className="my-20 bg-gray-100">
+      <section className='my-20 bg-gray-100'>
         <Benefits data={work_with_us_benefits} />
       </section>
       {/* Hiring Process */}
-      <section className="new-container">
-        <div className="text-center sm:w-1/2 mx-auto">
-          <h1 className="text-4xl font-semibold">Our hiring process</h1>
-          <p className="mt-2 mb-5 font-light">
+      <section className='new-container'>
+        <div className='text-center sm:w-1/2 mx-auto'>
+          <h1 className='text-4xl font-semibold'>Our hiring process</h1>
+          <p className='mt-2 mb-5 font-light'>
             We have a 5-step hiring process that generally takes about 2-4 weeks
             from application to offer letter.
           </p>
         </div>
-        <div className="mx-auto md:w-1/2 mb-10">
+        <div className='mx-auto md:w-1/2 mb-10'>
           {hiring_process.map(({ image, heading, descripition }, index) => (
-            <div className="md:flex mb-10" key={index}>
-              <div className="min-w-[64px] max-w-[64px] min-h-[64px] max-h-[64px] bg-blue-200  rounded-full">
-                <img
-                  src={image}
-                  layout="fill"
-                />
+            <div className='md:flex mb-10' key={index}>
+              <div className='min-w-[64px] max-w-[64px] min-h-[64px] max-h-[64px] bg-blue-200  rounded-full'>
+                <img src={image} layout='fill' />
               </div>
-              <div className="mt-3 md:mt-0 md:ml-4">
-                <h2 className="font-semibold text-xl">{heading}</h2>
-                <p className="font-light">{descripition}</p>
+              <div className='mt-3 md:mt-0 md:ml-4'>
+                <h2 className='font-semibold text-xl'>{heading}</h2>
+                <p className='font-light'>{descripition}</p>
               </div>
             </div>
           ))}
-          <div className="text-center">
+          <div className='text-center'>
             <a
-              href="https://in.indeed.com/cmp/Hcode-Technologies/jobs"
-              target="_blank"
-              className="primary-button text-ce"
+              href='https://in.indeed.com/cmp/Hcode-Technologies/jobs'
+              target='_blank'
+              className='primary-button text-ce'
             >
               See All Open Positions
             </a>
@@ -164,30 +168,28 @@ function work_with_us() {
         </div>
       </section>
       {/* FAQ */}
-      <div className="text-center bg-gray-100 pt-0 pb-20 md:my-20">
+      <div className='text-center bg-gray-100  md:my-20'>
         <Accordion data={faq_hiring} />
-
       </div>
 
-      {/* TODO Open Position */}
       {/* Are your Recuriter */}
-      <section className="new-container py-10">
-        <div className="grid md:grid-cols-2">
+      <section className='new-container pt-0 pb-10'>
+        <div className='grid md:grid-cols-2'>
           <img
             src={work_with_us_recuriter_image}
-            layout="fill"
-            className="rounded-lg"
+            layout='fill'
+            className='rounded-lg'
           />
-          <div className="my-4 md:my-auto md:w-4/5 ml-3">
-            <h1 className="text-4xl font-semibold">Are you a recruiter?</h1>
-            <p className="text-lg mt-2 mb-8">
+          <div className='my-4 md:my-auto md:w-4/5 ml-3'>
+            <h1 className='text-4xl font-semibold'>Are you a recruiter?</h1>
+            <p className='text-lg mt-2 mb-8'>
               If you are recruiter looking to help us with our hiring needs,
               please get in touch with us below.
             </p>
             <a
-              href="mailto:hr@hcode.tech"
-              target="_blank"
-              className="primary-button text-ce"
+              href='mailto:hr@hcode.tech'
+              target='_blank'
+              className='primary-button text-ce'
             >
               Get In Touch
             </a>
