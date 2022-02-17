e30ace78d91e6099913a9bdf1c2ecbfda382798c
Aman <aman@Amans-MacBook-Air.local>
2022-02-17 17:22:20 +0530

    udpate the stylesheet

--- pages/stylesheet.js
+++ pages/stylesheet.js
@@ -1,29 +1,33 @@
-import React from 'react'
+import React from "react";
 
 const stylesheet = () => {
-    return (
-        <div>
-            <div className="bg-[#F0E9FF] text-[#FFF]"></div>
-            <div className="bg-[#FFE8CF] text-[#FFFFFF]"></div>
-            <div className="bg-[#000000] text-[#000]"></div>
-            <div className="bg-[#EBECFF] text-[#000000]"></div>
-            <div className="bg-[#333333] text-[#fff]"></div>
-            <div className="bg-[#190830] text-[#ffffff]"></div>
-            <div className="bg-[#343D52] text-[#485063]"></div>
-            <div className="bg-[#343D52] text-[#111111]"></div>
-            <div className="bg-[#6BA564]"></div>
-            <div className="bg-[#111625]"></div>
-            <div className="bg-[#878E92]"></div>
-            <div className="bg-[#82AACD]"></div>
-            <div className="bg-[#87A9A6]"></div>
-            <div className="bg-[#F9F0EC]"></div>
-            <div className="bg-[#F2F2ED]"></div>
-            <div className="bg-[#FBFBFB]"></div>
-            <div className="bg-[#fff]"></div>
-            <div className="bg-[#fff]"></div>
-            <div className="bg-[#fff]"></div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <div className="bg-[#F0E9FF] text-[#FFF]"></div>
+      <div className="bg-[#FFE8CF] text-[#FFFFFF]"></div>
+      <div className="bg-[#000000] text-[#000]"></div>
+      <div className="bg-[#EBECFF] text-[#000000]"></div>
+      <div className="bg-[#333333] text-[#fff]"></div>
+      <div className="bg-[#190830] text-[#ffffff]"></div>
+      <div className="bg-[#343D52] text-[#485063]"></div>
+      <div className="bg-[#111111] text-[#111111]"></div>
+      <div className="bg-[#6BA564]"></div>
+      <div className="bg-[#111625]"></div>
+      <div className="bg-[#878E92]"></div>
+      <div className="bg-[#82AACD]"></div>
+      <div className="bg-[#87A9A6]"></div>
+      <div className="bg-[#F9F0EC]"></div>
+      <div className="bg-[#F2F2ED]"></div>
+      <div className="bg-[#4C4CE9]"></div>
+      <div className="bg-[#19082F]"></div>
+      <div className="bg-[#F1877A]"></div>
+      <div className="bg-[#50485B]"></div>
+      <div className="bg-[#FFE2AB]"></div>
+      <div className="bg-[#4884EC]"></div>
+      <div className="bg-[#5869FF]"></div>
+      <div className="bg-[#FBFBFB]"></div>
+    </div>
+  );
+};
 
-export default stylesheet
+export default stylesheet;
