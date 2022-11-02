37317284da90dcaec8e27fe5cb5fb790ffde955e
Aditya Chaudhary <adityachaudhary@Adityas-MacBook-Air.local>
2022-11-02 13:18:32 +0530

    added blog in toggle button

--- components/Navbar.js
+++ components/Navbar.js
@@ -272,6 +272,17 @@ export default function Navbar() {
               >
                 Our Work
               </a>
+              <a
+                href="https://hcode.tech/blog/" target="_blank"
+                className={` border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium
+                    ${
+                      pathname === "/blog"
+                        ? "bg-blue-50 border-blue-500 text-blue-700"
+                        : ""
+                    }   `}
+              >
+                Blog
+              </a>
 
               {/* {navbar_links.map(({ label, link }) => {
                 return (
