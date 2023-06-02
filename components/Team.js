f9df8abbfc1200a85cedff1fcf386222c018e554
Aman <108462474+amanhcode@users.noreply.github.com>
2023-06-02 14:19:06 +0530

    responsive issue fix (#58)

--- components/Team.js
+++ components/Team.js
@@ -1,33 +1,39 @@
-import { linkedin_icon } from "../constants/constants";
+import { linkedin_icon } from '../constants/constants';
 function Team({ name, image, image_webp, bio, link, title }) {
   return (
-    <div className="mb-5 w-[227px] mx-auto">
-      <div className="relative   mb-2">
+    <div className="mb-5 w-[227px] md:w-full mx-auto">
+      <div className="relative text-center mx-auto  mb-2">
         <picture>
-          <source 
-          type="image/webp"
-          srcSet={image_webp} 
-          media="(min-width:250px)"
+          <source
+            type="image/webp"
+            srcSet={image_webp}
+            media="(min-width:250px)"
           />
-        <img
-          className="object-cover md:h-[280px] min-w-full xl:min-w-[275px]"
-          src={image}
-          layout="fill"
-          loading="lazy"
-          height='280px'
-          width='220'
-          alt='Leadership team and mentors in hcode technologies'
-        />
-
+          <img
+            className="object-cover md:h-[280px] min-w-full xl:min-w-[275px]"
+            src={image}
+            layout="fill"
+            loading="lazy"
+            height="280px"
+            width="220"
+            alt="Leadership team and mentors in hcode technologies"
+          />
         </picture>
-    
+
         <div className="p-2 absolute bottom-4  left-2 cursor-pointer bg-gray-100 rounded-full hover:bg-white">
           <a href={link} target="_blank">
-            <img src={linkedin_icon} alt={link} className="w-4 md:w-full" width='15px' height='15px' loading='lazy' />
+            <img
+              src={linkedin_icon}
+              alt={link}
+              className="w-4 md:w-full"
+              width="15px"
+              height="15px"
+              loading="lazy"
+            />
           </a>
         </div>
       </div>
-      <div className="md:w-[227px]">
+      <div className="w-full px-5 text-center mx-auto">
         <h4 className="text-lg md:text-xl font-semibold  ">{name}</h4>
         <h6 className="text-md font-medium text-gray-400">{title}</h6>
         <p className="text-sm md:text-base font-light">{bio}</p>
