e9ae073f2c7d3b8c1bccbdd13cbc92b555358d73
Aman <aman.goyal@hcode.tech>
2023-08-24 15:44:07 +0530

    updated tracking code on the website

--- pages/_document.js
+++ pages/_document.js
@@ -27,6 +27,7 @@ class MyDocument extends Document {
             }}
           />
 
+          {/* Shashank Google analytics account */}
           <script
             async
             src="https://www.googletagmanager.com/gtag/js?id=G-V751ZVXNMP"
@@ -34,24 +35,21 @@ class MyDocument extends Document {
           <script
             dangerouslySetInnerHTML={{
               __html: ` window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-V751ZVXNMP');`,
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-V751ZVXNMP');`,
             }}
           />
+          {/* Shashank Google analytics account */}
 
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-6LJ1JVZYH6"
-          ></script>
+          {/* GTAG */}
           <script
             dangerouslySetInnerHTML={{
-              __html: `  window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-      
-        gtag('config', 'G-6LJ1JVZYH6');`,
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5XWGPH57')`,
             }}
           />
 
@@ -71,15 +69,25 @@ class MyDocument extends Document {
             type="text/javascript"
             dangerouslySetInnerHTML={{
               __html: `
-          (function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)}
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y)
-        })(window, document, "clarity", "script", "fkqnyaxcv1");`,
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "ik50r7bw2q");`,
             }}
           />
         </Head>
         <body>
+          {/* <!-- Google Tag Manager (noscript) --> */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-5XWGPH57"
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            ></iframe>
+          </noscript>
+          {/* <!-- End Google Tag Manager (noscript) --> */}
           <Main />
           <NextScript />
         </body>
