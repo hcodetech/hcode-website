c9dc4fbce70e827338246d3e8f69b44bc1a0b0cb
Mainka <mainka.dhankher@hcode.tech>
2023-12-28 11:15:18 +0530

    change newsletter popup timing

--- pages/_app.js
+++ pages/_app.js
@@ -20,7 +20,7 @@ function MyApp({ Component, pageProps }) {
     if (stroredData === null) {
       setTimeout(() => {
         setShowNewsLetterPopup(true);
-      }, [60000]);
+      }, [15000]);
     }
     sessionStorage.setItem("isSubscriptionPopupVisible", showNewsLetterPopup);
   }, []);
