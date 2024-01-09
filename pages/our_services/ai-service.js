32faa37efdca0e59b92ae92312c4575b11f6c747
Aman <108462474+amanhcode@users.noreply.github.com>
2024-01-09 14:06:46 +0530

    Feature/ai page (#74)
    * ai-page
    
    * ai-page
    
    * ai-page
    
    * ai-page
    
    * image changes
    
    * hero image change
    
    * ai-page-bgimage
    
    * changed bg images
    
    * ai page
    
    * updated ai blog
    
    * removed unused images
    
    * pr comments resolved
    
    * blog section pading
    
    * faq data name error
    
    * faq data error
    
    ---------
    
    Co-authored-by: abhay kushwaha <abhay.kushwaha@hcode.tech>
    Co-authored-by: Mainka <mainka.dhankher@hcode.tech>

--- pages/our_services/ai-service.js
+++ pages/our_services/ai-service.js
@@ -16,7 +16,7 @@ import {
   ai_services,
   ai_technologies,
   client,
-  faq_ai,
+  faqAi,
   metaData,
 } from "../../constants/constants";
 import { getTestimonialData } from "../../utils/config";
@@ -175,7 +175,7 @@ const AIService = () => {
 
       {/* FAQ */}
       <section className="bg-gray-100">
-        <Accordion data={faq_ai} />
+        <Accordion data={faqAi} />
       </section>
 
       {/* CTA */}
