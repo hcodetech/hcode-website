5013e0ed523df4aedc4007b29a7ba550dbf24aef
rohan <rohan.kamboj@hcode.tech>
2024-12-03 19:11:23 +0530

    implemented recaptcha v3

--- pages/_app.js
+++ pages/_app.js
@@ -1,46 +1,57 @@
-import "../node_modules/slick-carousel/slick/slick.css";
-import "../node_modules/slick-carousel/slick/slick-theme.css";
-import "tailwindcss/tailwind.css";
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
-import "../styles/global.scss";
-import { useEffect, useState } from "react";
-import NewsletterPopUp from "../components/NewsletterPopUp";
+import '../node_modules/slick-carousel/slick/slick.css';
+import '../node_modules/slick-carousel/slick/slick-theme.css';
+import 'tailwindcss/tailwind.css';
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+import '../styles/global.scss';
+import { useEffect, useState } from 'react';
+import NewsletterPopUp from '../components/NewsletterPopUp';
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 
 function MyApp({ Component, pageProps }) {
   const [showNewsLetterPopup, setShowNewsLetterPopup] = useState(false);
   let url;
-  if (typeof window !== "undefined") {
+  if (typeof window !== 'undefined') {
     url = window.location.pathname;
   }
 
   useEffect(() => {
-    const stroredData = sessionStorage.getItem("isSubscriptionPopupVisible");
+    const stroredData = sessionStorage.getItem('isSubscriptionPopupVisible');
 
     if (stroredData === null) {
       setTimeout(() => {
         setShowNewsLetterPopup(true);
       }, [15000]);
     }
-    sessionStorage.setItem("isSubscriptionPopupVisible", showNewsLetterPopup);
+    sessionStorage.setItem('isSubscriptionPopupVisible', showNewsLetterPopup);
   }, []);
 
   return (
-    <div>
-      {showNewsLetterPopup && (
-        <NewsletterPopUp
-          showNewsLetterPopup={showNewsLetterPopup}
-          setShowNewsLetterPopup={setShowNewsLetterPopup}
-        />
-      )}
+    <GoogleReCaptchaProvider
+      reCaptchaKey='6LftNJEqAAAAAOY_6Lt0mG1g1UimgCqGIPP4pA0j'
+      // scriptProps={{
+      //   async: false,
+      //   defer: false,
+      //   appendTo: 'head',
+      //   nonce: undefined,
+      // }}
+    >
+      <div>
+        {showNewsLetterPopup && (
+          <NewsletterPopUp
+            showNewsLetterPopup={showNewsLetterPopup}
+            setShowNewsLetterPopup={setShowNewsLetterPopup}
+          />
+        )}
 
-      <div className="bg-white">
-        {<Navbar />}
+        <div className='bg-white'>
+          {<Navbar />}
 
-        <Component {...pageProps} />
-        {url !== "/newsletter" && <Footer />}
+          <Component {...pageProps} />
+          {url !== '/newsletter' && <Footer />}
+        </div>
       </div>
-    </div>
+    </GoogleReCaptchaProvider>
   );
 }
 
